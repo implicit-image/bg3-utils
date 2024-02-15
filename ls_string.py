ae3b5294000c8c34684eb48bcd02ed49fc2bae53
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-02-15 08:27:11 +0100

    Implement LSString update method

--- ls_string.py
+++ ls_string.py
@@ -5,9 +5,9 @@ class LSString:
 
     def __init__(self, string: str | None):
         if string is None: return None
-        self.tokens = string.split(";")
+        tokens = string.split(";")
         expressions = {}
-        for expr in self.tokens:
+        for expr in tokens:
             #ActionResource(BardicInspiration,3,0)
             #expr_type = ActionResource
             #arg = BardicInspiration,3,0
@@ -22,12 +22,11 @@ class LSString:
                     expressions[expr_type][func] = [arg_params]
                 else:
                     expressions[expr_type][func] = []
-
             elif len(arg_params) != 0:
                 expressions[expr_type] = { func : [arg_params] }
             else:
                 expressions[expr_type] = { func: [] }
-        print(expressions)
+        # print(expressions)
         self.expressions = expressions
 
     def __getitem__(self, item):
@@ -36,29 +35,28 @@ class LSString:
     def __setitem__(self, item, value):
         self.expressions[item] = value
 
-    # TODO: implement this properly
-    def __add__(self, other):
-        one = str(self)
-        other = str(other)
-        result = LSString(one + ";" + other)
-        return result
+    def update(self, other):
+        for expr_type, expr in other.expressions.items():
+            # if we do not habe given expression type, just add it
+            if expr_type not in self.expressions.keys():
+                self.expressions[expr_type] = expr
+            # if we do, merge sub-dicts
+            else:
+                self.expressions[expr_type].update(other.expressions[expr_type])
+
 
     def __str__(self):
         result = []
         for expr_type, expr_func in self.expressions.items():
-            print(f"EXPR TYPE: {expr_type}")
-            print(f"EXPR FUNC: {expr_func}")
-            if type(expr_func) is not dict or type(expr_func) is not list:
+            if type(expr_func) is not dict and type(expr_func) is not list:
                 result.append(f"{expr_type}({expr_func})")
                 continue
             for func, params in expr_func.items():
-                print(f"FUNC: {func}")
                 # SavingThrow, [['Charisma'], ['Dexterity']
                 if len(params) == 0:
                     expr_str = f"{expr_type}({func})"
                     result.append(expr_str)
                 else:
-                    print(f"PARAMS: {params}")
                     for param_table in params:
                         # ['Charisma']
                         if len(param_table) >= 1:
@@ -72,12 +70,3 @@ class LSString:
 
 
 
-# l = "ActionResource(BardicInspiration,3,0);ActionResource(SpellSlot,4,1);\
-# ProficiencyBonus(SavingThrow,Dexterity);ProficiencyBonus(SavingThrow,Charisma)\
-# ;Proficiency(LightArmor);Proficiency(SimpleWeapons);Proficiency(HandCrossbows);Proficiency(Longswords);Proficiency(Rapiers);Proficiency(Shortswords);Proficiency(MusicalInstrument);Attribute(UseMusicalInstrumentForCasting)"
-# string = LSString("ActionResource(BardicInspiration,3,0);ActionResource(SpellSlot,4,1);ProficiencyBonus(SavingThrow,Dexterity);ProficiencyBonus(SavingThrow,Charisma);Proficiency(LightArmor);Proficiency(SimpleWeapons);Proficiency(HandCrossbows);Proficiency(Longswords);Proficiency(Rapiers);Proficiency(Shortswords);Proficiency(MusicalInstrument);Attribute(UseMusicalInstrumentForCasting)")
-# print(string)
-# print(str(string) == l)
-# print(string["ActionResource"])
-# string["ActionResource"]["SpellSlot"] = [['7', '0']]
-# print(string)
--- progressions.py
+++ progressions.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 from sys import argv
 from pathlib import Path
 from utils import intersperse, dict_contains, dict_filter
+from ls_string import LSString
 
 
 class Progressions:
@@ -25,7 +26,7 @@ class Progressions:
     def __split_LSS_string(self, lss_string: str) -> list[str] | None:
         return lss_string.split(";")
 
-    def get_nodes_by_class(self, class_name):
+    def get_nodes_by_class(self, class_name: str):
         return self.xml_tree_root.iterfind(f"./region/node/children//node/attribute[@value='{class_name}']/..")
 
 
@@ -38,17 +39,26 @@ class Progressions:
     # self.update(string, "ActionResource(SpellSlot,4,1)", "ActioResource(SpellSlot,8,1)")
     #
 
-    def modify_node_attribute_value(self, node: ET.Element, attr_id: str, value: str, OP_FLAG="update") -> ET.Element | None:
+    def modify_node_attribute_value(self, node: ET.Element, attr_id: str, value: str, value_name : str, OP_FLAG="update") -> ET.Element | None:
         attr_elem = node.find(f"./attribute[@id={attr_id}]")
         if attr_elem:
             if OP_FLAG == "overwrite":
                 attr_elem.set("value", value)
             elif OP_FLAG == "update":
-                lss_string = attr_elem.get("value")
-                updated_lss_string = self.update_lss_string(lss_string, value_name, value)
-            attr_elem.set("value", updated_lss_string)
+                ls_string = LSString(attr_elem.get("value"))
+                if not ls_string: return None
+                ls_string[value_name] = value
+                attr_elem.set("value", str(ls_string))
             return node
         return None
+
+    def get_node_lvl(self, node: ET.Element) -> int | None:
+        level = node.find(f".//attribute[@id='Level']")
+        if level is None: return -1
+        if level is None:
+            return -1
+        level = level.get("value")
+        return int(level)
 
 
     def get_nodes_by_level(self, level):
@@ -63,28 +73,64 @@ class Progressions:
                 nodes_by_level.append(node)
         return nodes_by_level
 
-    def get_attr_elem(self, node: ET.Element, attr_data: dict[str, str]) -> ET.Element | None:
+    def get_attr_elem_by_attrs(self, node: ET.Element, attr_data: dict[str, str]) -> ET.Element | None:
         for child in node:
             if dict_contains(child.attrib, attr_data):
                 return child
         return None
 
+    def get_attr_elem_by_id(self, node: ET.Element, attr_id: str) -> ET.Element | None:
+        return self.get_attr_elem_by_attrs(node, { "id" : attr_id })
+
     def make_attribute_elem(self, attr_name: str) -> ET.Element:
         return ET.Element('attribute', self.progressionAttributes[attr_name])
 
-    def add_spell_slots(self, classes: list[str], level: int, slot_level: int, slots: int):
-        cls = {}
+    def add_spell_slots(self, classes: list[str], levels: list[int], slots: int, slot_level = -1, ):
         for char_class in classes:
-            cls[char_class] = []
+            class_nodes = self.get_nodes_by_class(char_class)
+            for node in class_nodes:
+                if self.get_node_lvl(node) not in levels:
+                    continue
+                boosts = self.get_attr_elem_by_id(node, "Boosts")
+                if boosts is None: continue
+                ls_string = LSString(boosts.get("value"))
+                classSlot = "SpellSlot"
+                if char_class == "Warlock":
+                    classSlot = "WarlockSpellSlot"
+                slot_info = ls_string['ActionResource'][classSlot][0]
+                curr_slot_level = slot_info[0]
+                curr_num_of_slots = slot_info[1]
+                print(curr_slot_level, curr_num_of_slots)
+                ls_string['ActionResource'][classSlot][0] = [[int(curr_num_of_slots) + slots]]
+                boosts.set("value", str(ls_string))
+
+    # ActionResource(SpellSlot,2,4)
+    def add_boost_to_node(self, boost, node):
+        boosts_node = self.get_attr_elem_by_id(node, "Boosts")
+        if boosts_node is None: return -1
+        ls_string = LSString(boosts_node.get("value"))
+        # if node already has this boost, return -1
+        if boost in list(ls_string.expressions.keys()):
+            return -1
+        boost_ls_string = LSString(boost)
+        ls_string.append(boost_ls_string)
+        boosts_node.set("value", str(ls_string))
+
+    def make_boost(self, boost_type: str, boost_args: list[str]):
+        LSString(f"{boost_type}()")
 
 
-    
+
     def test(self):
-        bard_node = list(self.get_nodes_by_class('Bard'))[0]
-        lss_string = self.get_attr_elem(bard_node, { "id" : "Boosts" })
-        lss_string = lss_string.get('value')
-        if not lss_string: return 0
-        lss_string = self.update_lss_string(lss_string, "ActionResource(SpellSlot,4,1)", "ActionResource(SpellSlot,8,1)")
+        self.add_spell_slots(["Rogue"], [1, 2, 3], 4, 4)
+        nodes = []
+        for node in nodes:
+            boosts_el = self.get_attr_elem_by_id(node, "Boosts")
+            if boosts_el is None: continue
+            value = boosts_el.get("value")
+            ls_string = LSString(value)
+            if ls_string is None: continue
+            boosts_el.set("value", str(ls_string))
 
 
     def write(self):
